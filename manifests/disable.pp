e5eb86159e88572d66aad6a4197fae5f9d2db4ef
que <qs5779@mail.com>
2019-01-29 21:54:23 -0500

    disable support for ipv6

--- manifests/disable.pp
+++ manifests/disable.pp
@@ -1,21 +1,52 @@
 # ----------
 # Remove and Disable the DHCP server
 # ----------
-class dhcp::disable {
-  include ::dhcp::params
+class dhcp::disable (
+  Boolean $ipv4 = true,
+  Boolean $ipv6 = true,
+){
 
-  $packagename = $::dhcp::params::packagename
-  $servicename = $::dhcp::params::servicename
+  if $ipv4 or $ipv6 {
 
-  package { $packagename:
-    ensure => absent,
+    include ::dhcp::params
+
+    $packagename = $::dhcp::params::packagename
+    $servicename = $::dhcp::params::servicename
+    $servicename6 = $::dhcp::params::servicename6
+
+    if $ipv4 and $ipv6 {
+      package { $packagename:
+        ensure => absent,
+      }
+      $services = unique([$servicename],[$servicename6])
+      $require = Package[$packagename]
+    }
+    elsif $ipv4 {
+      $services = $servicename == $servicename6 ? {
+        true    => undef,
+        default => $servicename,
+      }
+      $require = undef
+    }
+    else { # it has to be ipv6
+      $services = $servicename == $servicename6 ? {
+        true    => undef,
+        default => $servicename6,
+      }
+      $require = undef
+    }
+
+    if $services != undef {
+      service { $services:
+        ensure    => stopped,
+        enable    => false,
+        hasstatus => true,
+        require   => $require,
+      }
+    }
+
   }
-
-  service { $servicename:
-    ensure    => stopped,
-    enable    => false,
-    hasstatus => true,
-    require   => Package[$packagename],
+  else {
+    notfiy { 'dchp::disable nothing to do need ipv4 or ipv6 parameter true': }
   }
-
 }
